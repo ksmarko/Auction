8e654bae0954079c40340f7e3dbda357b0bd88af
Ksenia <ks.markodev@gmail.com>
2018-05-14 10:25:32 +0300

    Cleared project

--- DAL/EF/DataContext.cs
+++ DAL/EF/DataContext.cs
@@ -33,7 +33,6 @@ namespace DAL.EF
                     VALUES ('f0e55c71-2e6c-41df-bea1-1ff5dc102f59' ,'admin@gmail.com' ,0 ,'AGWw56+JXBJWPXED3BLTjDmPM5/166S/TuwnjGG842sBb1a967GidG+05UWJRukNXw==' ,'1c52b5dd-622b-47a6-b426-46e4751c548b',NULL ,0 ,0 ,NULL ,0 ,0 ,'admin@gmail.com')", //password = "adminpassword"
                 @"INSERT INTO [dbo].[Users] ([Id], [Name]) VALUES ('f0e55c71-2e6c-41df-bea1-1ff5dc102f59', 'Admin')",
                 @"INSERT INTO [dbo].[AspNetUserRoles] ([UserId] ,[RoleId]) VALUES ('f0e55c71-2e6c-41df-bea1-1ff5dc102f59', '9c39f82e-c87b-4bfb-b786-eec87ac45744')",
-                @"INSERT INTO [dbo].[Categories] ([Name]) VALUES ('Other')",
                 @"INSERT INTO [dbo].[Categories] ([Name]) VALUES ('Auto parts, Tuning, GPS')",
                 @"INSERT INTO [dbo].[Categories] ([Name]) VALUES ('Audio, Radio')",
                 @"INSERT INTO [dbo].[Categories] ([Name]) VALUES ('Business, Industry, Services')",
@@ -44,7 +43,8 @@ namespace DAL.EF
                 @"INSERT INTO [dbo].[Categories] ([Name]) VALUES ('Cottage, Garden, Backyard')",
                 @"INSERT INTO [dbo].[Categories] ([Name]) VALUES ('House, Interior')",
                 @"INSERT INTO [dbo].[Categories] ([Name]) VALUES ('Plants')",
-                @"INSERT INTO [dbo].[Categories] ([Name]) VALUES ('Antiques')"
+                @"INSERT INTO [dbo].[Categories] ([Name]) VALUES ('Antiques')",
+                @"INSERT INTO [dbo].[Categories] ([Name]) VALUES ('Other')"
             };
 
             foreach (var el in commands)
