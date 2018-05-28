6ef332dd996052946d1f0d00ebac9d20a8002cd4
Ksenia <ks.markodev@gmail.com>
2018-05-28 14:28:09 +0300

    Creared PL

--- WebApi/Global.asax.cs
+++ WebApi/Global.asax.cs
@@ -1,12 +1,6 @@
 ﻿using AutoMapper;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 using WebApi.App_Start;
 
@@ -20,8 +14,8 @@ namespace WebApi
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
             GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("multipart/form-data"));
+
             Mapper.Initialize(cfg =>
             {
                 BLL.Infrastructure.AutoMapperConfig.Configure(cfg);
